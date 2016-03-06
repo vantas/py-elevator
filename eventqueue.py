0ed21dd225b4b911ec111f73748fd22e03ebf467
Pedro Vanzella <pedro@pedrovanzella.com>
2016-03-06 14:45:00 -0300

    Add EventQueue

---
+++ eventqueue.py
@@ -0,0 +1,12 @@
+class EventQueue:
+    def __init__(self):
+        self.eventQueue = []
+
+    def push(self, event):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
